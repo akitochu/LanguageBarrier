51222841918b52fa5cb9125b20bca2fefb5b0929
Akito Oyama <akitochu@gmail.com>
2022-10-18 20:35:56 +0100

    incorporate socket to send and recieve messages to and from the server. working chat between multiple windows

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,17 +1,35 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import MessageLog from "./MessageLog"; 
 import { v4 as uuidv4 } from 'uuid';
+const { io } = require("socket.io-client");
+
+let socket = null;
 
 function App() {
   const [messages, setMessages] = useState([])
   const messageRef = useRef()
+  useEffect(() => {
+    if (!socket){
+      socket = io.connect('http://localhost:3000');
+      socket.on('message-from-others', function(message){
+      console.log('Received ' + message);
+      addMessage(message)
+  })
+    }
+  }, []);
+
   function sendMessage(e) {
     const message = messageRef.current.value
     if (message === "") return
-    setMessages(prevTodos => {
-      return [...prevTodos, {id: uuidv4(), original: message}]
-    })
+    addMessage(message)
+    socket.emit('codeboard-message', message);
     messageRef.current.value = null
+  }
+
+  function addMessage(message) {
+    setMessages(prevMessages => {
+      return [...prevMessages, {id: uuidv4(), original: message}]
+    })
   }
 
   return (
