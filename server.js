4718d8fc8a2a129e56c42c40b154d1ce6bc7a4da
Akito Oyama <akitochu@gmail.com>
2022-11-10 15:19:10 +0000

    store users and chatlog so that it can be broadcast when a new user joins replace existing username with a new username if the user decides to change it

--- server.js
+++ server.js
@@ -10,17 +10,50 @@ app.get('/chat', (req, res) => {
 });
 
 let userCount = 0;
+let users = [];
+let chatLog = [];
 
 io.on('connection', (socket) => {
+  socket.broadcast.emit('update-users', users)
   console.log('User Online');
+  let thisUsername;
   socket.on('send-username', (username) => {
-    userCount += 1;
-    console.log(userCount);
-    socket.broadcast.emit('new-user', userCount);
+    socket.emit('check-for-updating-username', (username))
   });
+  socket.on('new-username', (username) => {
+    userCount += 1;
+    users.push(username);
+    thisUsername = username
+    socket.emit('new-user', userCount, users)
+    socket.broadcast.emit('new-user', userCount, users);
+  });
+  socket.on('update-username', (username, oldUsername) => {
+    for (var i=0; i < users.length; i++){
+      if (users[i] === oldUsername){
+        users.splice(i, 1);
+        i--;
+      }
+    }
+    users.push(username);
+    thisUsername = username
+    socket.emit('new-user', userCount, users)
+    socket.broadcast.emit('new-user', userCount, users);
+  })
   socket.on('codeboard-message', (msg) => {
-    console.log('message: ' + msg);
-	socket.broadcast.emit('message-from-others', msg);
+    if (msg === "uptodate") {
+      console.log(chatLog)
+      if (chatLog.length === 0){
+        console.log("chatlog empty")
+        return
+      }else{
+        console.log("gotinif")
+        socket.emit('message-from-others', chatLog)
+      }
+    }else{
+      chatLog.push(msg);
+      console.log(chatLog);
+	    socket.broadcast.emit('message-from-others', msg);
+    }
   });
   socket.on('disconnect', function(data) {
     for (var i=0; i < users.length; i++){
